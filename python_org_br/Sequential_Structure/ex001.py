5053d579bb6525ac0aab2226e80a51b53dedef44
Marcos Henrique Silva Quirino <marcoshsq@gmail.com>
2022-02-28 18:11:50 -0300

    Update ex001.py

--- python_org_br/Sequential_Structure/ex001.py
+++ python_org_br/Sequential_Structure/ex001.py
@@ -1,4 +1,4 @@
-# Extra Exercise:
+# Extra Exercise 001
 '''Make a program that asks for a number and then displays the message The number entered was [number].'''
 
 num = float(input('Insira um valor: '))
