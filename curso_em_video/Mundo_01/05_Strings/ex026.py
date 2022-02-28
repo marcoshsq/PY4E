0239abadc2bfac1fa12b4226fa39fece7cea3f6d
Marcos Henrique Silva Quirino <marcoshsq@gmail.com>
2022-02-28 18:07:01 -0300

    Update ex026.py

--- curso_em_video/Mundo_01/05_Strings/ex026.py
+++ curso_em_video/Mundo_01/05_Strings/ex026.py
@@ -1,6 +1,6 @@
-# Exercício Python 026 - Primeira e última ocorrência de uma string
-'''Faça um programa que leia uma frase pelo teclado e mostre quantas vezes aparece a letra "A", 
-em que posição ela aparece a primeira vez e em que posição ela aparece a última vez.'''
+# Python Exercise 026 - First and Last Occurrence of a String
+'''Make a program that reads a sentence on the keyboard and shows how many times the letter "A" appears,
+what position it first appears in and what position it appears last time.'''
 
 name = input('Insert name: ').casefold().strip()
 num_a = name.count('a') 
