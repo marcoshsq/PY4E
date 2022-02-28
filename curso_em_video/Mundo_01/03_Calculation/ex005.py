2c5f91845bdb44dc35eecf9f3c4c2b5554e1fdb7
Marcos Henrique Silva Quirino <marcoshsq@gmail.com>
2022-02-28 17:56:41 -0300

    Update ex005.py

--- curso_em_video/Mundo_01/03_Calculation/ex005.py
+++ curso_em_video/Mundo_01/03_Calculation/ex005.py
@@ -1,4 +1,4 @@
-# Exercise Python 005: Before and after
+# Python Exercise 005: Before and after
 '''Write a program that reads an integer and displays its successor and predecessor on the screen'''
 
 num = float(input('Insira um número: '))
