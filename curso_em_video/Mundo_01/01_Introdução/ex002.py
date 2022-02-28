d0afb9cb94e4b1b674a5887a74f20fec78331bc2
Marcos Henrique Silva Quirino <marcoshsq@gmail.com>
2022-02-28 17:25:18 -0300

    Update ex002.py

--- curso_em_video/Mundo_01/01_Introdução/ex002.py
+++ curso_em_video/Mundo_01/01_Introdução/ex002.py
@@ -1,10 +1,6 @@
 # Exercise Python 002: Answer to the user
 '''Make a program that reads a person's name and displays a welcome message.'''
 
-
-
-
-
 nome = input('Insert your name: ')
 print(f'Hi {nome}, nice to meet you!')
 
