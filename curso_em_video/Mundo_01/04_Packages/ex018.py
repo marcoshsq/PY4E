565624f46bfab293ce347afa377da7f050b0a40a
Marcos Henrique Silva Quirino <marcoshsq@gmail.com>
2022-02-28 18:01:39 -0300

    Update ex018.py

--- curso_em_video/Mundo_01/04_Packages/ex018.py
+++ curso_em_video/Mundo_01/04_Packages/ex018.py
@@ -1,6 +1,6 @@
-# Exercício Python 018 - Seno, Cosseno e Tangente
-''' Faça um programa que leia um ângulo qualquer e mostre na tela o valor do seno, 
-cosseno e tangente desse ângulo.'''
+# Python Exercise 018 - Sine, Cosine and Tangent
+''' Write a program that reads any angle and displays the sine value on the screen,
+cosine and tangent of this angle.'''
 
 import math
 
