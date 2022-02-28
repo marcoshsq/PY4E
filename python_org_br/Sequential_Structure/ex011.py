be3073b0110e3b614488aae1aa19364e9d1d6d22
Marcos Henrique Silva Quirino <marcoshsq@gmail.com>
2022-02-28 18:16:12 -0300

    Update ex011.py

--- python_org_br/Sequential_Structure/ex011.py
+++ python_org_br/Sequential_Structure/ex011.py
@@ -1,6 +1,6 @@
-# Exercício Extra:
-'''Faça um programa que peça o tamanho de um arquivo para download (em MB) e a velocidade de um link de Internet (em Mbps), 
-calcule e informe o tempo aproximado de download do arquivo usando este link (em minutos).'''
+# Extra Exercise 011
+'''Make a program that asks for the size of a download file (in MB) and the speed of an Internet link (in Mbps),
+calculate and report the approximate download time of the file using this link (in minutes).'''
 
 
 file_size = float(input('Insert the size of the file (MB):'))
