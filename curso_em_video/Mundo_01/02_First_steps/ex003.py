fbde4eb2cc439b1bef80d8aa98e9b2cc9dc99b1a
Marcos Henrique Silva Quirino <marcoshsq@gmail.com>
2022-02-28 17:27:46 -0300

    Update ex003.py

--- curso_em_video/Mundo_01/02_First_steps/ex003.py
+++ curso_em_video/Mundo_01/02_First_steps/ex003.py
@@ -1,12 +1,12 @@
-# Exercício Python 003: Somando dois números.
-'''Crie um programa que leia dois números e mostre a soma entre eles.'''
+# Exercise Python 003: The sum of two values.
+'''Create a program that reads two numbers and displays the sum between them.'''
 
-num_1 = int(input('Insira um valor: '))
-num_2 = int(input('Insira um segundo valor: '))
+num_1 = int(input('Insert the first value: '))
+num_2 = int(input('Insert the second value: '))
 sum = num_1 + num_2
-print(f'A soma entre {num_1} e {num_2} é {sum}')
+print(f'The sum between {num_1} and {num_2} is {sum}')
 
 
-''' Esse f junto com as mascaras {} é bem útil, bom deixar treinar essa sintaxe
-para melhorar meu código u.u'''
+''' This f along with the {} masks is very useful, good to let you train this syntax
+to improve the code u.u'''
 
