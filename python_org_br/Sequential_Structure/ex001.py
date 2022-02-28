2a3941c5e3b945f3f36dd4f27f593cefd6bd77e5
Marcos Henrique Silva Quirino <marcoshsq@gmail.com>
2022-02-28 18:10:33 -0300

    Update ex001.py

--- python_org_br/Sequential_Structure/ex001.py
+++ python_org_br/Sequential_Structure/ex001.py
@@ -1,5 +1,5 @@
-# Exercício Extra: 
-'''Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número].'''
+# Extra Exercise:
+'''Make a program that asks for a number and then displays the message The number entered was [number].'''
 
 num = float(input('Insira um valor: '))
 print(f'O valor informado {num}')
