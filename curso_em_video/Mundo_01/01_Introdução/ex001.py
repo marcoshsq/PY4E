0785f15f9156dc7d0d7624f36fc84c4eb5092d8f
Marcos Henrique Silva Quirino <marcoshsq@gmail.com>
2022-02-28 17:20:38 -0300

    Update ex001.py

--- curso_em_video/Mundo_01/01_Introdução/ex001.py
+++ curso_em_video/Mundo_01/01_Introdução/ex001.py
@@ -2,4 +2,7 @@
 
 '''Create a program that prints "Hello, World" on the screen'''
 
-print('Hello, World!') # print is the function that prints stuff u.u
+print('Hello, World!') 
+
+
+# print is the function that prints stuff u.u
