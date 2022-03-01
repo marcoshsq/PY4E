e8e0cca259c8f921f864bdfc4f388b0383cc807b
Marcos Henrique Silva Quirino <marcoshsq@gmail.com>
2022-02-28 23:55:21 -0300

    Update

--- lets_code/ex003.py
+++ lets_code/ex003.py
@@ -1,1 +1,16 @@
 # Extra Exercise 003: let's Code
+
+'''Let's make a program to verify who is the murderer of a crime.
+To find the killer, the police make a short quiz with 5
+questions where the answer can only be yes or no:
+
+A. Do they live near the victim?
+B. Have they ever worked with the victim?
+C. Did they called the victim?
+D. Were they at the crime scene? 
+E. Did they owe the victim?
+
+Each yes answer gives one point to the suspect. The police consider that
+suspects with 5 points are murderers, with 4 to 3 points are accomplices and
+2 points are only suspect, needing further investigation. Values
+equal to or below 1 are released.'''
--- lets_code/ex004.py
+++ lets_code/ex004.py
@@ -1,1 +1,4 @@
 # Extra Exercise 004: let's Code
+
+'''Write a program that prints the 9 times table (from 9*1 to 9*10) using loops.'''
+
