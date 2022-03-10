49e3c61cef0f9b151a6e2291ac1d5147996732b3
Marcos Henrique <marcoshsq@gmail.com>
2022-03-10 03:31:34 -0300

    Update ex047.py

---
+++ Main_list/02_Comandos_Intermediários/08_Repetitions_(for)/ex047.py
@@ -0,0 +1,6 @@
+# Exercício Python 047 - Contagem de pares
+
+'''Crie um programa que mostre na tela todos os números pares que estão no intervalo entre 1 e 50.'''
+
+for i in range(2, 51, 2):
+    print(i, end=' ') # Ver o que esse end= significa u.u
