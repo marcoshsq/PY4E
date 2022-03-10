c61bbb2eb23de5fa465e244713b03a3ba76fe305
Marcos Henrique <marcoshsq@gmail.com>
2022-03-10 03:28:33 -0300

    Update ex004.py

--- Extra_list_01/ex004.py
+++ Extra_list_01/ex004.py
@@ -1,3 +1,14 @@
 # Exercício Extra 004: let's Code
 
 '''Escreva um programa que imprima a tabuada dos 9 (de 9*1 a 9*10) usando loops.'''
+
+# Usando o for:
+
+num = int(input('Digite um número: '))
+print(f'A tabuada do {num} é:')
+print('=' * 13)
+for i in range(1, 11) :
+    print(num , 'x', i, '=', num * i )
+
+print('=' * 13)
+
