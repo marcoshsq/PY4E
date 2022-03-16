70079e414cb96c85c5f1789d4ea175599f57eac1
Marcos Henrique <marcoshsq@gmail.com>
2022-03-16 03:21:01 -0300

    Update ex002.py

--- Main_list/01_Fundamentals/01_Intro/ex002.py
+++ Main_list/01_Fundamentals/01_Intro/ex002.py
@@ -1,22 +1,5 @@
 # Exercise 002: Answering the User.
 '''Ask the user for his name and give him a welcome message.'''
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# Solution:
-
 name = input('What\'s is your name? ')          
 print(f'Hi {name}, nice to meet you!')     
