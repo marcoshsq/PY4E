124fc83ab31c6fa44f9d3a445bb62679a1554343
Marcos Henrique <marcoshsq@gmail.com>
2022-03-19 18:06:10 -0300

    Exercise 018 - solved

--- 01_Python_Crash_Course/01_Fundamentals/04_Packages/ex018.py
+++ 01_Python_Crash_Course/01_Fundamentals/04_Packages/ex018.py
@@ -1,31 +1,29 @@
-# Exercício Python 018 - Seno, Cosseno e Tangente
-''' Faça um programa que leia um ângulo qualquer e mostre na tela o valor do seno, 
-cosseno e tangente desse ângulo.'''
-
 import math
+# Exercise 018 - Sine, Cosine and Tangent
+''' Write a program that reads any angle and displays the sine, cosine and tangent value on the screen of this angle.'''
 
-# Em graus radianos
+# in degrees radians
 
-an = float(input('Insira o valor do ângulo: '))
+an = float(input('Enter the angle: '))
 ang = math.radians(an)
 sen_rad = math.sin(ang)
 cos_rad = math.cos(ang)
 tan_rad = math.tan(ang)
 
-print(f'O seno é: {sen_rad:.2f} O cosseno é: {cos_rad:.2f} e a tangente é: {tan_rad:.2f}')
+print(f'The Sine is: {sen_rad:.2f}. The Cosine is: {cos_rad:.2f} and the Tangent is: {tan_rad:.2f}')
 
-# Em graus centigrados
+# in degrees centigrade
 
 sen_deg = sen_rad * (180/math.pi)
 cos_deg = cos_rad * (180/math.pi)
 tan_deg = tan_rad * (180/math.pi)
 
-print(f'O seno é: {sen_deg:.2f} O cosseno é: {cos_deg:.2f} e a tangente é: {tan_deg:.2f}')
+print(f'The Sine is: {sen_deg:.2f}. The Cosine is: {cos_deg:.2f} and the Tangent is: {tan_deg:.2f}')
 
-# ou 
+# or 
 
 seno = math.degrees(sen_rad)
 cosen = math.degrees(cos_rad)
 tang = math.degrees(tan_rad)
 
-print(f'O seno é: {seno:.2f} O cosseno é: {cosen:.2f} e a tangente é: {tang:.2f}')
+print(f'The Sine is: {seno:.2f}. The Cosine is: {cosen:.2f} and the Tangent is: {tang:.2f}')
