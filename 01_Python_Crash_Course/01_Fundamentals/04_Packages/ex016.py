918fd59a51338569bdc47e4140fe1035f342e802
Marcos Henrique <marcoshsq@gmail.com>
2022-03-19 17:29:46 -0300

    Exercise 016 - solved

--- 01_Python_Crash_Course/01_Fundamentals/04_Packages/ex016.py
+++ 01_Python_Crash_Course/01_Fundamentals/04_Packages/ex016.py
@@ -1,9 +1,15 @@
-#Exercício Python 016: Quebrando um número
-'''Crie um programa que leia um número Real qualquer pelo teclado e mostre na tela a sua porção Inteira.'''
+import math #As a matter of good practice, everything we import comes before the program
 
-import math
+#Exercise 016: Cracking a number
+'''Create a program that reads any Real number from the keyboard and displays its Integer portion.'''
 
-num = float(input('Digite um valor: '))
-print(f'O valor digitado foi {num}, e sua porção inteira é', math.trunc(num)) # Jeito 01 usando trunc
-print(f'O valor digitado foi {num}, e sua porção inteira é', math.floor(num)) # Jeito 02 usando floor
-print(f'O valor digitado foi {num}, e sua porção inteira é', int(num)) # Jeito 02 usando a função int()
+
+
+num = float(input('Enter a value: '))
+print(f'The value entered was {num}, and its integer portion is', math.trunc(num)) # Way 01 using trunc function
+print(f'The value entered was {num}, and its integer portion is', math.floor(num)) # Way 02 using floor function
+print(f'The value entered was {num}, and its integer portion is', int(num)) # Way 03 using type casting, int() function
+
+# floor function - round numbers down to the nearest integer
+# trunc function - find the integer part of different numbers
+
