2dc262127dcb1acaf29b7ca052efd93a2773094d
Marcos Henrique <marcoshsq@gmail.com>
2022-03-19 13:31:20 -0300

    Exercise 014 - solved

--- 01_Python_Crash_Course/01_Fundamentals/03_Calculation/ex014.py
+++ 01_Python_Crash_Course/01_Fundamentals/03_Calculation/ex014.py
@@ -1,40 +1,7 @@
-# Exercício Python 014: Conversor de temperaturas
-'''Escreva um programa que converta uma temperatura digitando em graus Celsius 
-e converta para graus Fahrenheit.'''
+# Exercise 014: Temperature converter
+'''Write a program that converts a temperature by typing in degrees Celsius
+and convert to degrees Fahrenheit.'''
 
-# Conversor de Celcius
-
-while True:
-    try :
-        unity_choice = input('Choose: 1) ° Celcius to ° Farhenheit 2) ° Celcius to °Kelvin : ')
-        celcius = float(unity_choice)
-
-# Celcius to Farhenheit
-
-        if celcius == 1 :
-            c_to_f = input('Put a value: ')
-            c_to_f_value = float(c_to_f)
-            result_c_to_f = (c_to_f_value * 9 / 5) + 32
-            print(f'{c_to_f}° celcius is {result_c_to_f}° farhenheit')
-            break
-
-# Celcius to Kelvin
-
-        elif celcius == 2 :
-            c_to_k = input('Put a value: ')
-            c_to_k_value = float(c_to_k)
-            result_c_to_k = c_to_k_value + 273.15
-            print(f'{c_to_k}° celcius is {result_c_to_k}° kelvin')
-            break
-
-        elif celcius > 2 :
-            print('Choose 1 or 2')
-
-# Update one day 
-
-    except :
-        print('Updating Soon')
-        break
-
-    else :
-        continue
+celsius = float(input("Enter the temperature in Celsius: \n"))
+farenheit = ((1.8 * celsius) + 32)
+print(f'{celsius}ºC correspond to {farenheit:.1f}ºF.')
