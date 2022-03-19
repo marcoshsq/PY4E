455431f6230eb3c07f9f9628b09448f702a9f06c
Marcos Henrique <marcoshsq@gmail.com>
2022-03-19 20:00:18 -0300

    Exercise 027 - solved

--- 01_Python_Crash_Course/01_Fundamentals/05_Strings/ex027.py
+++ 01_Python_Crash_Course/01_Fundamentals/05_Strings/ex027.py
@@ -1,8 +1,8 @@
-# Exercício Python 027 - Primeiro e último nome de uma pessoa
-'''Faça um programa que leia o nome completo de uma pessoa, 
-mostrando em seguida o primeiro e o último nome separadamente.'''
+# Exercise 027 - A person's first and last name
+'''Make a program that reads a person's full name,
+then showing the first and last name separately.'''
 
 name = input('Insert name: ')
 first_name = name.split()[0]
 last_name = name.split()[-1]
-print(f'Olá, prazer em conhece-lo sr. {last_name}, ou posso chama-lo de {first_name}')
+print(f'Hi. nice to meet you mr. {last_name}, or I should call you {first_name}')
