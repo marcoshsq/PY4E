76ef0e11477eabc3bc7ba1b7dc5b2cb7c3ea3227
Marcos Henrique <marcoshsq@gmail.com>
2022-03-19 13:29:38 -0300

    Exercise 012 - solved

--- 01_Python_Crash_Course/01_Fundamentals/03_Calculation/ex012.py
+++ 01_Python_Crash_Course/01_Fundamentals/03_Calculation/ex012.py
@@ -1,8 +1,8 @@
-# Exercício Python 012: Calculando descontos
-''' Faça um algoritmo que leia o preço de um produto e mostre seu novo preço, com 5% de desconto.'''
+# Exercise 012: Calculating discounts
+'''Make an algorithm that reads the price of a product and displays its new price, with a 5% discount.'''
 
-preço = float(input('Qual o valor do produto: '))
-desc = preço * 0.95
-print(f'O produto que custava R${preço}, na promoção, com 5% off, ficou R${desc:.2f}')
+price = float(input('Product\'s price: '))
+discount = price * 0.95
+print(f'The product that cost R${price}, in the promotion, with 5% off, is R${discount:.2f}')
 
-'''This (:.2f) limits the number of places after the comma, but I'm not sure how it works yet u.u'''
+#This (:.2f) limits the number of places after the comma u.u
