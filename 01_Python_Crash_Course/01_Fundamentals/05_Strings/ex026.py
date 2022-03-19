ebd0c0c6c38ec17c47e660d192c220c0cc46a9fd
Marcos Henrique <marcoshsq@gmail.com>
2022-03-19 19:58:53 -0300

    Exercise 026 - solved

--- 01_Python_Crash_Course/01_Fundamentals/05_Strings/ex026.py
+++ 01_Python_Crash_Course/01_Fundamentals/05_Strings/ex026.py
@@ -1,12 +1,12 @@
-# Exercício Python 026 - Primeira e última ocorrência de uma string
-'''Faça um programa que leia uma frase pelo teclado e mostre quantas vezes aparece a letra "A", 
-em que posição ela aparece a primeira vez e em que posição ela aparece a última vez.'''
+# Exercise 026 - First and last occurrence of a string
+'''Make a program that reads a sentence on the keyboard and shows how many times the letter "A" appears,
+what position it first appears in and what position it appears last time.'''
 
 name = input('Insert name: ').casefold().strip()
 num_a = name.count('a') 
 first_a = name.find('a')
 last_a = name.rfind('a')
 
-print(f'A letra A aparece {num_a} vezes na frase')
-print(f'A primeira letra A aparece na posição {first_a + 1}')
-print(f'A ultima letra A apareceu na posição {last_a + 1}')
+print(f'The letter a appears {num_a} times')
+print(f'The first appearence is at {first_a + 1}')
+print(f'The last is at {last_a + 1}')
