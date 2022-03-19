1553dd6363c61ab6d30d4e54a4b5f9138aeee5aa
Marcos Henrique <marcoshsq@gmail.com>
2022-03-19 13:28:21 -0300

    Exercise 10 - solved

--- 01_Python_Crash_Course/01_Fundamentals/03_Calculation/ex010.py
+++ 01_Python_Crash_Course/01_Fundamentals/03_Calculation/ex010.py
@@ -1,7 +1,7 @@
-# Exercício Python 010: Conversor de moedas
-'''Crie um programa que leia quanto dinheiro uma pessoa tem na carteira 
-e mostre quantos dólares ela pode comprar.'''
+# Exercise 010: Currency Converter 
+'''Create a program that reads how much money a person has in their wallet,
+then show how many dollars he can buy.'''
 
-money_real = float(input('Qual quantia você possui: '))
+money_real = float(input('How much money do you have: '))
 dolar = money_real / 5.10
-print(f'Com R${money_real} você pode comprar US${dolar:.2f} ') 
+print(f'With R${money_real} you can buy US${dolar:.2f} ') 
