0234e4cfe207cf3fa2a238ee77a87a0393553ccf
Marcos Henrique <marcoshsq@gmail.com>
2022-03-19 18:17:20 -0300

    Exercise 021 - solved

--- 01_Python_Crash_Course/01_Fundamentals/04_Packages/ex021.py
+++ 01_Python_Crash_Course/01_Fundamentals/04_Packages/ex021.py
@@ -1,11 +1,10 @@
-# Exercício Python 021: Tocando um MP3
-'''Faça um programa em Python que abra e reproduza o áudio de um arquivo MP3.'''
+# Exercise 021: Playing a song
+'''Help python sing.'''
 
 import pygame
 pygame.init()
-pygame.mixer.music.load("arquivo_de_música.mp3")
+pygame.mixer.music.load("music_file.mp3")
 pygame.mixer.music.play()
 input()
 
-'''Aparentemente, para fazer o programar tocar uma música é necessarios
-fazer um input'''
+
