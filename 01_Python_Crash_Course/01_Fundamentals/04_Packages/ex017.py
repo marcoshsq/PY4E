a0c81af7826098267b75fc24900f09cccfd7e7d5
Marcos Henrique <marcoshsq@gmail.com>
2022-03-19 17:41:26 -0300

    Exercise 017 - solved

--- 01_Python_Crash_Course/01_Fundamentals/04_Packages/ex017.py
+++ 01_Python_Crash_Course/01_Fundamentals/04_Packages/ex017.py
@@ -1,23 +1,30 @@
-# Exercício Python 017: Catetos e hipotenusa 
-'''Faça um programa que leia o comprimento do cateto oposto 
-e do cateto adjacente de um triângulo retângulo. 
-Calcule e mostre o comprimento da hipotenusa.'''
-
 import math
 
-# Método 01, sem o módulo:
+# Exercise 017: Right Triangle 
+'''Write a program that reads the length of the opposite side and the adjacent side of a right triangle.
+Calculate and display the length of the hypotenuse.'''
 
-cat_op = float(input('Insira o valor do Cateto Oposto: ')) 
-cat_ad = float(input('Insira o valor do Cateto Adjacente: '))
-hip = ((cat_op ** 2) + (cat_ad ** 2)) ** 0.5
-print(f'A hipotenusa do triangulo mede {hip:.2f} u.m. ')
+# To do this we will use the Pythagorean theorem: a^2 = b^2 + c^2
 
-# Método 02, com módulo:
 
-hipo = math.sqrt(math.pow(cat_op, 2) + math.pow(cat_ad, 2))
-print(f'A hipotenusa do triangulo mede {hipo:.2f} u.m ')
+# Method 01, without the module Math:
+# First we ask for the leg values
+leg_a = float(input('Enter the value of leg a: ')) 
+leg_b = float(input('Enter the value of leg b: '))
 
-# Outro método usando o módulo u.u
+# Then we do the Pythagorean theorem: sqrt((leg_a^2)+(leg_b^2))
+hyp = ((leg_a ** 2) + (leg_b ** 2)) ** 0.5
+print(f'The triangle hypotenuse measures {hip:.2f} m.u. ')
 
-hipot = math.hypot(cat_op, cat_ad)
-print(f'A hipotenusa do triangulo mede {hipot:.2f} u.m. ')
+# Method 02, with the module using pow function:
+
+hypo = math.sqrt(math.pow(leg_a, 2) + math.pow(leg_b, 2))
+print(f'The triangle hypotenuse measures {hypo:.2f} m.u. ')
+
+
+
+# Method 03 using the module with the hypotenuse function u.u
+
+hypot = math.hypot(leg_a, leg_b)
+print(f'The triangle hypotenuse measures {hypot:.2f} m.u. ')
+
