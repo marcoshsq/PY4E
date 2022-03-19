c95a34f890ef9d0a6d65085fd18656c4308d0022
Marcos Henrique <marcoshsq@gmail.com>
2022-03-19 19:55:06 -0300

    Exercise 024 - solved

--- 01_Python_Crash_Course/01_Fundamentals/05_Strings/ex024.py
+++ 01_Python_Crash_Course/01_Fundamentals/05_Strings/ex024.py
@@ -1,7 +1,7 @@
-# Exercício Python 024 - Verificando as primeiras letras de um texto
-'''Crie um programa que leia o nome de uma cidade 
-diga se ela começa ou não com o nome "SANTO".'''
+# Exercise 024 - Checking the first letters of a text
+'''Create a program that reads the name of a city
+say whether or not it starts with a specific name.'''
 
-name = str(input('Insira o nome da cidade: ')).strip().casefold()
-city = name.startswith('santo')
+name = str(input('Enter a name: ')).strip().casefold()
+city = name.startswith('specify_a_name_here')
 print(city)
