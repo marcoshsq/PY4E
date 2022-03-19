4e73bc34ff3e11e23627c32a8d840ba2ac300e0f
Marcos Henrique <marcoshsq@gmail.com>
2022-03-18 22:00:57 -0300

    Exercise 005 - solved

--- 01_Python_Crash_Course/01_Fundamentals/03_Calculation/ex005.py
+++ 01_Python_Crash_Course/01_Fundamentals/03_Calculation/ex005.py
@@ -1,7 +1,14 @@
-# Exercício Python 005: Antecessor e sucessor
-'''Faça um programa que leia um número Inteiro e mostre na tela o seu sucessor e seu antecessor'''
+# Exercise 005: Before and After
+'''Write a program that reads an integer and displays its successor and predecessor on the screen'''
 
-num = float(input('Insira um número: '))
-prev_num = num - 1
-next_num = num + 1
-print(f'Analizando o valor {num}, o seu antecessor é {prev_num} e seu sucessor é {next_num}')
+# First we're gonna ask the user to input a number
+num = float(input('Enter a number: ')) 
+
+# We get the predecessor by subtracting 1 from our variable num
+prev_num = num - 1 
+
+# We get the successor by ading 1 to our variable num
+next_num = num + 1                     
+
+# Then we just print everything
+print(f'Analyzing the value {num}, your predecessor is {prev_num} and his successor is {next_num}')
