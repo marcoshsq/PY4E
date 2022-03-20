2ce53f77d908a61654d33b46a60f5cb51927cc31
Marcos Henrique <marcoshsq@gmail.com>
2022-03-20 01:28:41 -0300

    Exercise 039 - solved

--- 01_Python_Crash_Course/02_Control_Structures/07_Conditionals_II_(If, Elif, Else)/ex039.py
+++ 01_Python_Crash_Course/02_Control_Structures/07_Conditionals_II_(If, Elif, Else)/ex039.py
@@ -5,17 +5,25 @@ from datetime import date
 '''Make a program that reads a young person's year of birth and reports, according to their age,whether he is still going to enlist in the military, 
 whether it's the exact time to enlist or whether it's past the time of enlistment. Your program should also show the time remaining or overdue..'''
 
+# First let's assign the current year to a variable
+# That's why we import datetime
+current_year = date.today().year
+
+# Now we ask the user for his birth year
+birth_year = int(input('Year of birth: '))
+
+# We get his age, using simple math
+age = current_year - birth_year
 
 
-ano_atual = date.today().year
-ano_nascimento = int(input('Ano de nascimento: '))
-idade = ano_atual - ano_nascimento
-if idade == 18 :
-    print(f'O candidato possuí {idade} anos, deve se apresentar IMEDIATAMENTE!')
-elif idade > 18 :
-    print(f'O candidato possuí {idade} anos, já passou do período de alistamento, você deveria ter se apresentado há {idade - 18} anos!')
-    print(f'Seu alistamento foi em {ano_atual - (idade - 18)}')
+if age == 18 :
+    print(f'You have {age} years old, you must report IMMEDIATELY!')
+
+elif age > 18 :
+    print(f'The candidate has {age} years past the enlistment period, you should have reported {age - 18} years ago!')
+    print(f'Your enlistment was in {current_year - (age - 18)}')
+
 else :
-    print('O candidato tem menos de 18 anos!')
-    print(f'Ele possui {idade} anos, faltam {18 - idade} anos para ele se apresentar!')
-    print(f'Seu alistamento será em {ano_atual + (18 - idade)}')
+    print('The candidate has less than 18 years!')
+    print(f'You have {age} years, {18 - age} years to go before enlisting!')
+    print(f'Your enlistment will be in {current_year + (18 - age)}')
