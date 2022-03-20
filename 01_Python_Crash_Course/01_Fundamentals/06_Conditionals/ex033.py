6547618e32b93c64b37b2cdacea609d886487be7
Marcos Henrique <marcoshsq@gmail.com>
2022-03-20 00:03:47 -0300

    Exercise 033 - solved

--- 01_Python_Crash_Course/01_Fundamentals/06_Conditionals/ex033.py
+++ 01_Python_Crash_Course/01_Fundamentals/06_Conditionals/ex033.py
@@ -1,22 +1,22 @@
-# Python Exercise 033 - Maior e menor.
-'''Crie um programa que leia três valores e diga qual o maior e qual o menor.'''
+# Exercise 033 - Largest and smallest.
+'''Create a program that reads three values and says which is the largest and which is the smallest.'''
 
 a, b, c = [int(x) for x in input("Please enter the numbers: ").split()]
 
 # For the largest
 
 if a > b and c :
-    print(f'O maior é {a}')
+    print(f'The largest is {a}')
 elif b > c and a :
-    print(f'O maior é {b}')
+    print(f'The largest is {b}')
 else :
-    print(f'O maior é {c}')    
+    print(f'The largest is {c}')    
 
 # For the smallest
 
 if a < b and c :
-    print(f'E o menor é {a}')
+    print(f'The smallest is {a}')
 elif b < c and a :
-    print(f'E o menor é {b}')
+    print(f'The smallest is {b}')
 else :
-    print(f'O menor é {c}')
+    print(f'The smallest is {c}')
