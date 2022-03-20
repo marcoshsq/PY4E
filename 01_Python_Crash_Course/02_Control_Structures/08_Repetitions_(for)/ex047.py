068dd0be042a7bb528eea657f06a3cac9b61137a
Marcos Henrique <marcoshsq@gmail.com>
2022-03-20 16:47:59 -0300

    Exercise 047 - solved

--- 01_Python_Crash_Course/02_Control_Structures/08_Repetitions_(for)/ex047.py
+++ 01_Python_Crash_Course/02_Control_Structures/08_Repetitions_(for)/ex047.py
@@ -1,6 +1,14 @@
-# Exercício Python 047 - Contagem de pares
+# Exercise 047 - Counting evens
 
-'''Crie um programa que mostre na tela todos os números pares que estão no intervalo entre 1 e 50.'''
+'''Create a program that displays on the screen all even numbers that are in the range between 1 and 50.'''
 
+# We star at 2 because if we put zero it'll star at zero
+# If we put one, it will start at one, then jump two, to three, then 5... and so on up to 50, and we don't want odds, just evens
+# Since the range function has an index, it'll always ignore the last number that's why we put 51 at the finish
 for i in range(2, 51, 2):
-    print(i, end=' ') # Ver o que esse end= significa u.u
+    print(i, end=' ') 
+
+'''end='' is an argument of the print() function that contains a string to be inserted at the end of the line. 
+By default it contains the character \n which represents a newline, i.e. end='\n
+Since we're putting (end=' ') this command tell python to dysplay the values in just one line.
+'''
