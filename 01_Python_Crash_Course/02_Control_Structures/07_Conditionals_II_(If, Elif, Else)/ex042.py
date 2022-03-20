e72e11f30274369307b5c4fe1967a2231e937b84
Marcos Henrique <marcoshsq@gmail.com>
2022-03-20 01:47:23 -0300

    Exercise 042 - solved

--- 01_Python_Crash_Course/02_Control_Structures/07_Conditionals_II_(If, Elif, Else)/ex042.py
+++ 01_Python_Crash_Course/02_Control_Structures/07_Conditionals_II_(If, Elif, Else)/ex042.py
@@ -1,24 +1,31 @@
-# Exercício Python 042 - Analisando Triângulos v2.0
+# Exercise 042 - Analyzing Triangles v2.0
 
-'''Refaça o DESAFIO 035 dos triângulos, acrescentando o recurso de mostrar que tipo de triângulo será formado:
-- EQUILÁTERO: todos os lados iguais
-- ISÓSCELES: dois lados iguais, um diferente
-- ESCALENO: todos os lados diferentes'''
+'''Redo Exercise 035, the one of triangles, 
+adding the feature to show what type of triangle will be formed:
+    - EQUILATERAL: all sides equal
+    - ISOSCELES: two equal sides, one different
+    - SCALENO: all different sides'''
 
-'''Dados três segmentos de reta distintos, 
-se a soma das medidas de dois deles é sempre maior que a medida do terceiro, 
-então, eles podem formar um triângulo'''
+'''Given three distinct line segments, if the sum of the measures of two of them
+is always greater than the measure of the third, then they can form a triangle'''
 
-lado_a = float(input('Insira o valor do lado a do triângulo '))
-lado_b = float(input('Insira o valor do lado b do triângulo '))
-lado_c = float(input('Insira o valor do lado c do triângulo '))
+# First we ask for the sides of the triangles
+side_a = float(input('Insira o valor do lado a do triângulo '))
+side_b = float(input('Insira o valor do lado b do triângulo '))
+side_c = float(input('Insira o valor do lado c do triângulo '))
 
-if lado_a < lado_b + lado_c and lado_b < lado_c + lado_a and lado_c < lado_a + lado_c :
-    if lado_a == lado_b == lado_c :
+# Our first if, is to know if these values form one of the triangles we are looking for, for this we will use the and operator
+if side_a < side_b + side_c and side_b < side_c + side_a and side_c < side_a + side_b :
+
+    # If our first if is true we enter this second layer, this is a nested if
+    if side_a == side_b == side_c :
         print('Esse segmento forma um triângulo Equilátero!')
-    elif lado_a != lado_b != lado_c != lado_a :
+
+    elif side_a != side_b != side_c != side_a :
         print('Esse segmento forma um triângulo Escaleno!')
+        
     else :
         print('Esse segmento forma um triângulo Isóceles!')
+        
 else :
     print('Esse segmento não forma triângulo')
