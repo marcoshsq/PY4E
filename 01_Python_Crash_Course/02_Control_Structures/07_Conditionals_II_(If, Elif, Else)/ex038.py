d7ac7bcd970ff595eac81603fc37c5e402ef3834
Marcos Henrique <marcoshsq@gmail.com>
2022-03-20 01:17:11 -0300

    Exercise 038 - solved

--- 01_Python_Crash_Course/02_Control_Structures/07_Conditionals_II_(If, Elif, Else)/ex038.py
+++ 01_Python_Crash_Course/02_Control_Structures/07_Conditionals_II_(If, Elif, Else)/ex038.py
@@ -1,14 +1,17 @@
-# Exercício Python 038 - Comparando números
+# Exercise 038 - Comparing numbers
 
-'''Escreva um programa que leia dois números inteiros e compare-os. mostrando na tela uma mensagem:
-- O primeiro valor é maior'''
+'''Write a program that reads two integers and compares them. displaying a message on the screen: The first value is higher'''
 
-num_1 = int(input('Insira o primeiro valor: '))
-num_2 = int(input('Insira o segundo valor: '))
+# First we ask the user for the two numbers
+num_1 = int(input('Enter the first value: '))
+num_2 = int(input('Enter the second value: '))
 
+# We make simple comparisons
 if num_1 > num_2 :
-    print(f'O número {num_1} é maior que {num_2}')
+    print(f'The number {num_1} is bigger than {num_2}')
+
 elif num_2 > num_1 :
-    print(f'O número {num_2} é maior que {num_1}')
+    print(f'The number {num_2} is bigger than {num_1}')
+
 elif num_1 == num_2 :
-    print('Os valores são iguais')
+    print('The values are the same')
