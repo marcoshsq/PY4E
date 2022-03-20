1c45bdbf2b8a7f47757d98c8f47f6a438b3d875b
Marcos Henrique <marcoshsq@gmail.com>
2022-03-19 23:55:58 -0300

    Exercise 030 - solved

--- 01_Python_Crash_Course/01_Fundamentals/06_Conditionals/ex030.py
+++ 01_Python_Crash_Course/01_Fundamentals/06_Conditionals/ex030.py
@@ -1,9 +1,9 @@
-# Exercício Python 030 - Par ou Ímpar?
-'''Crie um programa que leia um número inteiro e mostre na tela se ele é PAR ou ÍMPAR.'''
+# Exercise 030 - Odd or Even?
+'''Create a program that reads an integer and shows on the screen whether it is ODD or EVEN.'''
 
 number = int(input('Insert a value: '))
 even_or_odd = number % 2
 if even_or_odd == 0 :
-    print(f'O número {number} é par')
+    print(f'The number {number} is odd')
 else :
-    print(f'O número {number} é impar')
+    print(f'The number {number} is even')
