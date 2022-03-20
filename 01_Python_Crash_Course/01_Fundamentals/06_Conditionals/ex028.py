4a32f6da92e7641b6b220877ae76b39e1c2b8661
Marcos Henrique <marcoshsq@gmail.com>
2022-03-19 23:46:50 -0300

    Exercise 028 - solved

--- 01_Python_Crash_Course/01_Fundamentals/06_Conditionals/ex028.py
+++ 01_Python_Crash_Course/01_Fundamentals/06_Conditionals/ex028.py
@@ -1,20 +1,21 @@
-# Exercício Python 028 - Jogo da Adivinhação v.1.0
-'''Escreva um programa que faça o computador "pensar" em um número inteiro entre 0 e 5 
-e peça para o usuário tentar descobrir qual foi o número escolhido pelo computador. 
-O programa deverá escrever na tela se o usuário venceu ou perdeu.'''
-
 import random
 import time
+# Exercise 028 - Guessing Game v.1.0
+'''Write a program that makes the computer "think" of an integer between 0 and 5
+and ask the user to try to find out which number was chosen by the computer.
+The program should write on the screen if the user won or lost.'''
+
+
 
 print('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=')
-print('Vou pensar em um número entre 0 e 5, tente adivinhar!')
+print('I\'ll think of a number between 0 and 5, try to guess!')
 print('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=')
 
-computer_guess = random.randint(0, 5) # Faz o computador escolher um número
-user_guess = int(input('Em que número eu pense? ')) # Número do jogador
-print('Processando...')   
-time.sleep(1)  # Essa funçãp vem com o pacote time e ele deixa eu colocar um time no meu código
+computer_guess = random.randint(0, 5) # Make the computer choose a number
+user_guess = int(input('What number did I think of? ')) 
+print('processing...')   
+time.sleep(1)  # This function comes with the time package and allows me put a little delay in the code
 if user_guess == computer_guess :
-    print('Parabéns, você acertou!')
+    print('Good job, you\'ve nailed !')
 else :
-    print ('Booh, Booh... ERROU!!!')
+    print ('Sorry, maybe next time!!!')
