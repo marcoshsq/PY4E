848aa36e1f16ab97db4c94666142915630743d78
Marcos Henrique <marcoshsq@gmail.com>
2022-03-20 17:22:14 -0300

    Exercise 048 - solved

--- 01_Python_Crash_Course/02_Control_Structures/08_Repetitions_(for)/ex048.py
+++ 01_Python_Crash_Course/02_Control_Structures/08_Repetitions_(for)/ex048.py
@@ -1,11 +1,21 @@
-# Exercício Python 048 - Soma ímpares múltiplos de três
+# Exercise 048 - Sum of odd multiples of three
 
-'''Faça um programa que calcule a soma entre todos os números que são múltiplos de três e que se encontram no intervalo de 1 até 500'''
+'''Write a program that calculates the sum of all numbers that are multiples of three and that lie in the range 1 to 500'''
 
-soma = 0        # Acumulador é esse valor para fazer contas
-contagem = 0    # Acumulador é esse valor para fazer contas
+# This is an accumulator, it'll help us build up a value across iterations of a loop
+sum = 0        
+# This is a tracker, it'll help us keep track of some value inside the loop
+cont = 0    
+
+# Here we're saying [start at 1, stop at 500, jump two by two]
 for i in range(1, 501, 2):
+    
     if i % 3 == 0:
-        contagem += 1    # Essa syntax de += diz que: ''a = a + b'' é igual á ''a += b'' 
-        soma += i
-print(f'A soma entre os {contagem} números é {soma}')
+        
+        # We add one to our tracker, because we want to count the number of iterarions it has made
+        cont += 1
+        # We add our control variable to sum, because in each loop, it'll receive a new value
+        sum += i
+
+# Our print go outside the loop, because we don't want to repeat it in each iteration        
+print(f'The sum between all the {cont} numbers is {sum}')
