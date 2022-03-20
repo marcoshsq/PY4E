fbdc1647d8c3f290b7009dc71703bed86d94d677
Marcos Henrique <marcoshsq@gmail.com>
2022-03-20 16:39:16 -0300

    Exercise 046 - solved

--- 01_Python_Crash_Course/02_Control_Structures/08_Repetitions_(for)/ex046.py
+++ 01_Python_Crash_Course/02_Control_Structures/08_Repetitions_(for)/ex046.py
@@ -1,15 +1,20 @@
-# Exercício Python 046 - Contagem regressiva
-
-'''Faça um programa que mostre na tela uma contagem regressiva para o estouro de fogos de artifício, 
-indo de 10 até 0, com uma pausa de 1 segundo entre eles.'''
-
 from time import sleep
 
-print('Contagem regressiva para os fogos!!!')
-sleep(1)
+# Exercise 046 - Countdown
+
+'''Write a program that displays a countdown on the screen,
+going from 10 to 0, with a 1 second pause between.'''
+
+print('It\'s the final Countdown!!!')
+
+sleep(1) # Function imported from time package
+
+# The range funtion has an index [star, finish, step]
 for i in range(10, -1, -1):
+    # This will print every iteration of the loop
+    # With an second of delay
     print(i)
     sleep(1)
-print('AEEEEEEEEEE')
 
-
+print('The final countdown')
+print('OHHHH, OHHHH')
