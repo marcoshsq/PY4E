5bc7bfc8e449ab400288b5e857986a82c2e865ff
Marcos Henrique <marcoshsq@gmail.com>
2022-03-20 00:06:26 -0300

    Exercise 034 - solved

--- 01_Python_Crash_Course/01_Fundamentals/06_Conditionals/ex034.py
+++ 01_Python_Crash_Course/01_Fundamentals/06_Conditionals/ex034.py
@@ -1,10 +1,10 @@
-# Exercício Python 034 - Aumentos múltiplos
-''' Escreva um programa que pergunte o salário de um funcionário e calcule o valor do seu aumento. 
-Para salários superiores a R$1250,00, calcule um aumento de 10%. 
-Para os inferiores ou iguais, o aumento é de 15%.'''
+# Exercise 034 - Multiple increases
+''' Write a program that asks for an employee's salary and calculates the amount of his raise:
+For salaries greater than R$1250.00, calculate a 10% increase.
+For those below or equal, the increase is 15%.'''
 
 wage = float(input('Insert a value: '))
 if wage >= 1250 :
-    print(f'O seu sálario que é R${wage:.2f} passou a ser R${wage * 1.1:.2f}, o valor do aumento foi de R${(wage * 1.1) - wage:.2f}')
+    print(f'Your salary is R${wage:.2f} became R${wage * 1.1:.2f}, the value of the increase was R${(wage * 1.1) - wage:.2f}')
 else :
-    print(f'O seu sálario que é R${wage:.2f} passou a ser R${wage * 1.15:.2f}, o valor do aumento foi de R${(wage * 1.15) - wage:.2f}')
+    print(f'Your salary is R${wage:.2f} became R${wage * 1.15:.2f}, the value of the increase was R${(wage * 1.15) - wage:.2f}')
