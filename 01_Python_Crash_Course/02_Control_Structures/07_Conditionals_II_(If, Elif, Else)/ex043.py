e03892d0c62dc1a1da320de3afcac0fafe0f1451
Marcos Henrique <marcoshsq@gmail.com>
2022-03-20 01:54:33 -0300

    Exercise 043 - solved

--- 01_Python_Crash_Course/02_Control_Structures/07_Conditionals_II_(If, Elif, Else)/ex043.py
+++ 01_Python_Crash_Course/02_Control_Structures/07_Conditionals_II_(If, Elif, Else)/ex043.py
@@ -1,23 +1,32 @@
-# Exercício Python 043 - Índice de Massa Corporal
+# Exercise 043 - Body Mass Index
 
-'''Desenvolva uma lógica que leia o peso e a altura de uma pessoa, calcule seu 
-Índice de Massa Corporal (IMC) e mostre seu status, de acordo com a tabela abaixo:
-- IMC abaixo de 18,5: Abaixo do Peso
-- Entre 18,5 e 25: Peso Ideal
-- 25 até 30: Sobrepeso
-- 30 até 40: Obesidade
-- Acima de 40: Obesidade Mórbida'''
+'''
+Develop a logic that reads a person's weight and height, calculates their
+Body Mass Index (BMI) and show your status according to the table below:
+    - BMI below 18.5: Underweight
+    - Between 18.5 and 25: Ideal Weight
+    - 25 to 30: Overweight
+    - 30 to 40: Obesity
+    - Above 40: Morbid Obesity
+    
+'''
 
-peso = float(input('Qual o seu peso (kg): '))
-altura = float(input('Qual a sua altura (m): '))
-imc = peso / (altura * altura)
-if imc < 18.5 :
-    print(f'Seu imc é {imc:.2f}, você está abaixo do peso.')
-elif 18.5 < imc < 25 :
-    print(f'Seu imc é {imc:.2f}, você está no peso ideal.')
-elif 25 < imc < 30 :
-    print(f'Seu imc é {imc:.2f}, você está acima do peso ideal.')
-elif 30 < imc < 40 :
-    print(f'Seu imc é {imc:.2f}, você está obeso.')
+weight = float(input('Qual o seu peso (kg): '))
+height = float(input('Qual a sua altura (m): '))
+
+bmi = weight / (height * height)
+
+if bmi < 18.5 :
+    print(f'Your BMI is {bmi:.2f}, you\'re Underweight.')
+
+elif 18.5 < bmi < 25 :
+    print(f'Your BMI is {bmi:.2f}, you have an Ideal Weight .')
+
+elif 25 < bmi < 30 :
+    print(f'Your BMI is {bmi:.2f}, you\'re Overweight.')
+
+elif 30 < bmi < 40 :
+    print(f'Your BMI is {bmi:.2f}, you\'re obese.')
+
 else :
-    print(f'Seu imc é {imc:.2f}, você está super obeso... wow')    
+    print(f'Your BMI is {bmi:.2f}, you\'re Morbid obese ... wow')   
