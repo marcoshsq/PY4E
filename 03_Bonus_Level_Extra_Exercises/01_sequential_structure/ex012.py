63bce36d4b1ed52a81407bcd7b8a6cdc2d924f6e
Marcos Henrique <marcoshsq@gmail.com>
2022-03-21 01:47:31 -0300

    Update ex012.py

--- 03_Bonus_Level_Extra_Exercises/01_sequential_structure/ex012.py
+++ 03_Bonus_Level_Extra_Exercises/01_sequential_structure/ex012.py
@@ -3,13 +3,13 @@
 '''Make a Program for a paint shop:
 
 The program should ask for the size in square meters of the area to be painted. 
-Assume that the paint coverage is 1 liter for every 6 square meters and that the paint is sold in 18 liter cans,
-that cost R$ 80.00 or in gallons of 3.6 liters, which cost R$ 25.00.
+Assume that the paint coverage is 1 liter for every 6 square meters and that the paint is sold in 18 liter gallons,
+that cost R$ 80.00 or in cans of 3.6 liters, which cost R$ 25.00.
 
 Inform the user of the quantities of ink to be purchased and the respective prices in 3 situations:
 
-a) buy only 18 liter cans;
-b) buy only 3.6 liter gallons;
+a) buy only 18 liter gallons;
+b) buy only 3.6 liter cans;
 c) mix cans and gallons, so that paint waste is less.
 
 Add 10% clearance and always round the values up, ie, consider full cans.'''
