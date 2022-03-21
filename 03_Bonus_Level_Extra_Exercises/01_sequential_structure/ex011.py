842da50863679bd6468bbbcfb996228ec5e1a76c
Marcos Henrique <marcoshsq@gmail.com>
2022-03-20 23:55:17 -0300

    Update ex011.py

--- 03_Bonus_Level_Extra_Exercises/01_sequential_structure/ex011.py
+++ 03_Bonus_Level_Extra_Exercises/01_sequential_structure/ex011.py
@@ -1,11 +1,10 @@
 # Extra Exercise 011
 
-'''Faça um programa que peça o tamanho de um arquivo para download (em MB) e a velocidade de um link de Internet (em Mbps), 
-calcule e informe o tempo aproximado de download do arquivo usando este link (em minutos).'''
+'''Make a program that asks for the size of a download file (in MB) and the speed of an Internet link (in Mbps), calculate and report the approximate download time of the file using this link (in minutes).'''
 
 
 file_size = float(input('Insert the size of the file (MB):'))
 internet_speed = float(input('Insert the velocity of your net (Mbps): '))
 bit_to_byte = internet_speed / 8
 download_time = (file_size / bit_to_byte) / 60
-print(f'O tempo de download será de {download_time:.2f}min')
+print(f'The download time will be {download_time:.2f}min')
