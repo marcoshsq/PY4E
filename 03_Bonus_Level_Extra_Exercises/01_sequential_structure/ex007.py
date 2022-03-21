8ed1b3d542e56fbadb398aa15d854228b2923b7e
Marcos Henrique <marcoshsq@gmail.com>
2022-03-20 23:48:51 -0300

    Update ex007.py

--- 03_Bonus_Level_Extra_Exercises/01_sequential_structure/ex007.py
+++ 03_Bonus_Level_Extra_Exercises/01_sequential_structure/ex007.py
@@ -1,10 +1,12 @@
-# Exercício Extra 007
+# Extra Exercise 007
 
-'''Faça um Programa que peça 2 números inteiros e um número real.
-Calcule e mostre:
-a) o produto do dobro do primeiro com metade do segundo .
-b) a soma do triplo do primeiro com o terceiro.
-c) o terceiro elevado ao cubo.'''
+'''Make a program that asks for 2 integers and a real number. Calculate and show:
+
+a) the product of the double of the first and half of the second.
+b) the sum of the triple of the first and the third.
+c) the third cubed.
+
+'''
 
 x , y , z = input('Insert three numbers: ').split()
 first_int = int(x)
@@ -15,6 +17,6 @@ a = (first_int * 2) * (second_int / 2)
 b = (first_int * 3) + real_num
 c = real_num ** 3
 
-print(f'O produto do dobro do primeiro com metade do segundo é: {a}')
-print(f'A soma do triplo do primeiro com o terceiroé: {b}')
-print(f'O terceiro elevado ao cubo é: {c}')
+print(f'The product of twice the first and half the second is: {a}')
+print(f'The sum of triple the first and third is: {b}')
+print(f'The third cubed is: {c}')
