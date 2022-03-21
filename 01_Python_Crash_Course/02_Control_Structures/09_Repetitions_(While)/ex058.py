1c5a0a0349f947d73a03bea58eb5d9393cfe2b8c
Marcos Henrique <marcoshsq@gmail.com>
2022-03-21 16:26:48 -0300

    Exercise 058 - solved

--- 01_Python_Crash_Course/02_Control_Structures/09_Repetitions_(While)/ex058.py
+++ 01_Python_Crash_Course/02_Control_Structures/09_Repetitions_(While)/ex058.py
@@ -1,31 +1,35 @@
-# Exercício Python #058 - Jogo da Adivinhação v2.0
-
-'''Melhore o jogo do DESAFIO 028 onde o computador vai "pensar" em um número entre 0 e 10. 
-Só que agora o jogador vai tentar adivinhar até acertar, mostrando no final quantos 
-palpites foram necessários para vencer.'''
-
 import random
 import time
 
+# Exercise 058 - Guessing Game v2.0
+
+'''Improve the game of exercise 028 where the computer will "think" of a number between 0 and 10.
+But now the player will try to guess until he gets it right, showing at the end how many
+guesses were needed to win.'''
+
 print('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=')
-print('Vou pensar em um número entre 0 e 10, tente adivinhar!')
+print('I\'ll think of a number between 0 and 10, try to guess!')
 print('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=')
 
-computer_guess = random.randint(0, 10) # Faz o computador escolher um número
-user_guess = int(input('Em que número eu pensei? ')) # Número do jogador
-print('Processando...')   
-time.sleep(1)  # Essa funçãp vem com o pacote time e ele deixa eu colocar um time no meu código
+computer_guess = random.randint(0, 10) 
+user_guess = int(input('What number did I think of? '))
+print('loading...')   
+time.sleep(1) 
+
+# Now, instead of using if, we'll use a while
 counter = 0
 while user_guess != computer_guess:
-    user_guess = int(input('Humm, não foi dessa vez. Por que não tenta de novo? '))
-    print('Processando...')   
+    user_guess = int(input('Hmm, not this time. Why don\'t you try again? '))
+    print('loading...')   
     time.sleep(1)
     counter += 1       
 
 
+# Here we will show how many attempts the player had to make
 if counter <= 1:
-    print(f'Parabéns, você é muito bom, só precisou de {counter} tentativa')
+    print(f'Congratulations, you are very good, just needed {counter} try')
 elif 1 < counter < 3:
-    print(f'Parabéns, você precisou de {counter} tentativas')
+    print(f'You\'re pretty good, needed only {counter} attempts')
 else:
-    print(f'Você conseguiu, precisou de {counter} tentativas, mas conseguiu.')
+    print(f'You got it, needed only {counter} attempts.')
+    
