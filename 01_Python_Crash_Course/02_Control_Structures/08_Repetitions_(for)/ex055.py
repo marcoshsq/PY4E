e0f3ab292b219dce219bdfaca3a9826d7fb5c021
Marcos Henrique <marcoshsq@gmail.com>
2022-03-20 22:58:29 -0300

    Exercise 055 - solved

--- 01_Python_Crash_Course/02_Control_Structures/08_Repetitions_(for)/ex055.py
+++ 01_Python_Crash_Course/02_Control_Structures/08_Repetitions_(for)/ex055.py
@@ -1,23 +1,41 @@
-# Exercício Python 055 - Maior e menor da sequência
+# Exercise 055 - Highest and lowest of the Sequence
 
-'''Faça um programa que leia o peso de cinco pessoas. No final, mostre qual foi o maior e o menor peso lidos.'''
+'''Make a program that reads the weight of five people. At the end, show which was the highest and lowest weight read.'''
 
 
-maior = 0
-menor = 0
+# 1. Trackers
+greater_weight = 0
+less_weight = 0
+question_counter = 0
 
-# Primeiro ler o peso das pessoas 5x
-for i in range(1, 6):
-    peso = float(input(f'Qual o peso da {i}º pessoa: ')) 
+# 2. Let's ask the user 5x
+for i in range(0, 5):
+    weight = float(input('Please, enter your weight: '))
+    question_counter += 1
+    print(f'You entered {weight} for the {question_counter}° person')
+
+# 3. Logic of our program
+
     if i == 1:
-        maior = peso
-        menor = peso
+        greater_weight = weight
+        less_weight = weight
+
     else:
-        if peso > maior:
-            maior = peso
-        elif peso < menor:
-            menor = peso
-    
-print(f"O maior peso é {maior}kg")
-print(f"O menor peso é {menor}kg.")  
+        if weight > greater_weight:
+            greater_weight = weight
+
+        elif weight < less_weight:
+            less_weight = weight
+
+
+# 4. We print everything
+print(f'The biggest weight is {greater_weight}kg')
+print(f'The smallest weight is {less_weight}kg')
+
+
+'''Imagine the following situation, I want you to tell me which is the highest and lowest number.
+I'll start: 7. Which is the biggest? 7 right, and the smallest, also 7, agree?.
+
+We've applied the same logic here, we're going to assume that our first loop is already the right one, 
+otherwise we'll replace it.'''
     
