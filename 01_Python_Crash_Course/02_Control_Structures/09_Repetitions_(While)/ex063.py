c685b27c7863e9c6dd48d6e714240000a3eb38db
Marcos Henrique <marcoshsq@gmail.com>
2022-03-21 21:18:27 -0300

    Exercise 063 - solved

--- 01_Python_Crash_Course/02_Control_Structures/09_Repetitions_(While)/ex063.py
+++ 01_Python_Crash_Course/02_Control_Structures/09_Repetitions_(While)/ex063.py
@@ -1,26 +1,30 @@
-# Exercício Python #063 - Sequência de Fibonacci v1.0
-'''Escreva um programa que leia um número N inteiro qualquer e 
-mostre na tela os N primeiros elementos de uma Sequência de Fibonacci. 
+# Exercise 063 - Fibonacci Sequence v1.0
 
-Ex: 0 - 1 - 1 - 2 - 3 - 5 - 8'''
+'''Write a program that reads any integer N and show on the screen the first N 
+elements of a Fibonacci Sequence.
+
+e.g.: 0 - 1 - 1 - 2 - 3 - 5 - 8'''
 
 print('=' * 30)
-print('Sequência de Fibonacci')
+print('Fibonacci Sequence')
 print('=' * 30)
-n = int(input('Quantos termos você quer mostrar: '))
+number = int(input('How many terms do you want to show: '))
 print('~' * 30)
 
-t1 = 0
-t2 = 1
+# As we already know that the first two terms are always the same (0, 1), we have already created them here.
+term_1 = 0
+term_2 = 1
 print('~' * 30)
-print(f'{t1} -> {t2}', end='')
+print(f'{term_1} -> {term_2}', end='')
 
-c = 3
-while c <= n:
-    t3 = t1 + t2
-    print(f' -> {t3} e', end='')
-    t1 = t2
-    t2 = t3
-    c += 1
-print(' FIM')
+# As we already have the first and second term, we will start our counter at 3
+counter = 3
+while counter <= number:
+    # At last, As the Fibonacci sequence is the sum of the two previous terms, we create this inside the loop
+    term_3 = term_1 + term_2
+    print(f' -> {term_3}', end='')
+    term_1 = term_2
+    term_2 = term_3
+    counter += 1
+print(' End')
 print('~' * 30)
