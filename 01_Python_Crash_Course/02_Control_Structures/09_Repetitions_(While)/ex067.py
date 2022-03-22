15a84dd2486cddf87731e68047e9ad8e9c25a02f
Marcos Henrique <marcoshsq@gmail.com>
2022-03-21 21:52:03 -0300

    Exercise 067 - solved

--- 01_Python_Crash_Course/02_Control_Structures/09_Repetitions_(While)/ex067.py
+++ 01_Python_Crash_Course/02_Control_Structures/09_Repetitions_(While)/ex067.py
@@ -1,14 +1,12 @@
-# Exercício Python #067 - Tabuada v3.0
+# Exercise 067 - Multiplication table v3.0
 
-''' Faça um programa que mostre a tabuada de vários números, um de cada vez, para cada valor digitado pelo usuário. 
-O programa será interrompido quando o número solicitado for negativo. '''
-
-counter = 1
+''' Write a program that displays the multiplication table of several numbers, one at a time, for each value entered by the user.
+The program will stop when the requested number is negative. '''
 
 while True:
-    n = int(input("Qual tabuada você quer?: \n"))
+    n = int(input('What table do you want?: \n'))
     if n < 0:
         break
     for i in range(1, 11):
-        print(f"{n} x {i} = {n * i}")        
-print("Programa encerrado.")
+        print(f'{n} x {i} = {n * i}')        
+print('End.')
