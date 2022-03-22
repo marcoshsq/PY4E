374762e85ba0285e65f44a7f2e798fbeca527c8e
Marcos Henrique <marcoshsq@gmail.com>
2022-03-21 21:25:32 -0300

    Exercise 064 - solved

--- 01_Python_Crash_Course/02_Control_Structures/09_Repetitions_(While)/ex064.py
+++ 01_Python_Crash_Course/02_Control_Structures/09_Repetitions_(While)/ex064.py
@@ -1,18 +1,20 @@
-# Exercício Python #064 - Tratando vários valores v1.0
-'''Crie um programa que leia vários números inteiros pelo teclado. O programa só vai parar quando 
-o usuário digitar o valor 999, que é a condição de parada. No final, mostre quantos números foram digitados 
-e qual foi a soma entre eles (desconsiderando o flag).'''
+# Exercise 064 - Handling Multiple Values v1.0
+
+'''Create a program that reads several integers from the keyboard. The program will only stop when
+the user enters the value 999, which is the stop condition. At the end, show how many numbers were entered
+and what was the sum between them (disregarding the flag).'''
 
 flag = False
-somador = 0
+sum = 0
 counter = 0
 
 while flag == False:
-    num = int(input("Digite um número [999 para parar]: \n"))
+    num = int(input('Enter a value [999 to stop]: '))
     if num == 999:
         flag = True
     else:
-        somador += num
+        sum += 1
         counter += 1
-print("Soma: {}.".format(somador))
-print("Números digitados: {}.".format(counter))
+
+print(f'You have entered {counter} numbers and the total sum between them is {sum}')
+
