bc715aaf7563c4510cfdd53eadc00226795108d0
Marcos Henrique <marcoshsq@gmail.com>
2022-03-21 22:55:46 -0300

    Exercise 068 - solved

--- 01_Python_Crash_Course/02_Control_Structures/09_Repetitions_(While)/ex068.py
+++ 01_Python_Crash_Course/02_Control_Structures/09_Repetitions_(While)/ex068.py
@@ -1,15 +1,75 @@
-# 1. We import the packages we are going to use, as we are going to make the computer choose something random, 
-# we need random, and we import the team, to give the game a thrill
-from random import randint
-from time import sleep
-
 # Exercise 068 - Odd or Even Game
 
 '''Make a program that plays odd or even with the computer.
 The game will only be stopped when the player loses,
 showing the total number of consecutive wins he had at the end of the game. '''
+
+# For better viewing:
+
+from random import randint
+from time import sleep
+
+victories = 0
+while True:
+    player = int(input('Enter a number betwenn 0 and 10 to play or 999 to stop: \n'))
+    if player < 0:
+        print('Enter a positive integer less than ten: ')
+    elif player > 10 and player != 999:
+        print("Enter an integer less than 10")
+    elif player == 999:
+        print('You decided to stop. I\'m sorry I\'m not so nice...(｡•́︿•̀｡)')
+        print(f'Consecutive wins: {victories}')
+        break
+    else:  
+        computer = randint(0, 10)
+        choice = str(input('Even or odd? [E / O] \n')).strip().upper()
+        if choice == "E":  
+            print('You chose even!')
+            sum = player + computer
+            if sum % 2 == 0:  
+                victories += 1
+                sleep(1)
+                print(f'The computer chose {computer}.')
+                print(f'{sum} it\'s even, so you\'ve won!')
+            else:  
+                sleep(1)
+                print(f'The computer chose {computer}.')
+                print(f'{sum} it\'s odd, so you\'ve loose!')
+                print(f'Consecutive wins: {victories}')
+                break
+        elif choice == "O":  
+            sum = player + computer
+            print('You chose odd!')
+            if sum % 2 != 0:  
+                victories += 1
+                sleep(1)
+                print(f'The computer chose {computer}.')
+                print(f'{sum} it\'s odd, so you\'ve won!')
+            else:  
+                sleep(1)
+                print(f'The computer chose {computer}.')
+                print(f'{sum} it\'s even, so you\'ve loose!')
+                print(f'Consecutive wins: {victories}')
+                break
+        else:
+            print('Please choose Even or odd!')
+            sleep(1.5)
+            print('Please (づ｡◕‿‿◕｡)づ')
+            sleep(1)
+
+
+
+
+
+
+#....
+
 # As always, this is going to be a pretty big program, so, let's make a script
 
+# 1. We import the packages we are going to use, as we are going to make the computer choose something random, 
+# we need random, and we import the team, to give the game a thrill
+from random import randint
+from time import sleep
 
 # 2. we want to show the number of times the player has won, so we will create an adder
 victories = 0
@@ -33,7 +93,7 @@ while True:
 
     # If the player wants to stop
     elif player == 999:
-        print('You decided to stop. I\'m sorry I\'m not so nice')
+        print('You decided to stop. I\'m sorry I\'m not so nice...(｡•́︿•̀｡)')
         print(f'Consecutive wins: {victories}')
         break
 
