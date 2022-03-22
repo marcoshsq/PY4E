a2cf2dc445b6753dbbc155022df46c40dd80cfe0
Marcos Henrique <marcoshsq@gmail.com>
2022-03-21 21:40:00 -0300

    Exercise 065 - solved

--- 01_Python_Crash_Course/02_Control_Structures/09_Repetitions_(While)/ex065.py
+++ 01_Python_Crash_Course/02_Control_Structures/09_Repetitions_(While)/ex065.py
@@ -1,20 +1,22 @@
-# Exercício Python #065 - Maior e Menor valores
-'''Crie um programa que leia vários números inteiros pelo teclado. No final da execução, 
-mostre a média entre todos os valores e qual foi o maior e o menor valores lidos. 
-O programa deve perguntar ao usuário se ele quer ou não continuar a digitar valores.'''
+# Exercise 065 - Largest and Smallest Values
+
+'''Create a program that reads several integers from the keyboard. At the end of execution,
+show the average of all values and what was the highest and lowest values read.
+The program should ask the user whether or not he wants to continue typing values.'''
 
 flag = False
-counter = adder = mean = highest = lowest = 0
+counter = sum = average = highest = lowest = 0
 
 while flag == False:
-    n = int(input("Digite um número inteiro: \n"))
+    n = float(input("Enter a number: \n"))
+    cont = str(input("Do you want to continue? [YES NO] \n")).strip().upper()
 
-    #  Média:
-    adder += n
+    #  Average:
+    sum += n
     counter += 1
-    mean = (adder / counter)
+    average = (sum / counter)
 
-    #  Maior/Menor:
+    # highest/lowest:
     if n > highest:
         highest = n
     if lowest == 0:
@@ -23,9 +25,8 @@ while flag == False:
         lowest = n
 
     #  Continuação:
-    cont = str(input("Quer continuar? [SIM / NÃO] \n")).strip().upper()
-    if cont == "NÃO":
+    if cont == "NO":
         flag = True
 
-print("Média: {:.1f}".format(mean))
-print("Maior: {} | Menor: {}".format(highest, lowest))
+print(f'Average: {average:.1f}')
+print(f'Highest {highest} / Lowest {lowest}')
