05e5cccfaf8d279013d1ba632dea21b5f46060a2
Marcos Henrique <marcoshsq@gmail.com>
2022-03-21 23:37:24 -0300

    Exercise #069 - solved

--- 01_Python_Crash_Course/02_Control_Structures/09_Repetitions_(While)/ex069.py
+++ 01_Python_Crash_Course/02_Control_Structures/09_Repetitions_(While)/ex069.py
@@ -1,27 +1,39 @@
-# Exercício Python #069 - Análise de dados do grupo
+# Exercise #069 - Group Data Analysis
 
-'''Crie um programa que leia a idade e o sexo de várias pessoas. 
-A cada pessoa cadastrada, o programa deverá perguntar se o usuário quer ou não continuar. 
-No final, mostre:
-A) quantas pessoas tem mais de 18 anos.
-B) quantos homens foram cadastrados.
-C) quantas mulheres tem menos de 20 anos. '''
+'''Create a program that reads the age and gender of multiple people.
 
-add_age = add_men = add_women = 0
+For each registered person, the program should ask if the user 
+wants to continue or not. At the end, show:
+
+A) how many people are over 18 years old.
+B) how many men were registered.
+C) how many women are under 20 years old. 
+'''
+
+# Let's star with our counters
+add_major = men = woman = 0
 
 while True:
-    age = int(input("Idade: \n"))
+
+    # We ask for age, and we add up those who are over 18
+    age = int(input('Enter age: \n'))
     if age >= 18:
-        add_age += 1
+        add_major += 1
+
+    # We ask for the sex, and we add up those who are men
     sex = str(input("Sexo: [M / F] \n")).strip().upper()
     if sex == "M":
-        add_men += 1
+        men += 1
+
+    # If they are not men, we see if they are women under 20 and we add
     elif sex == "F":
         if age < 20:
-            add_women += 1
-    choice = str(input("Continuar? [S / N] \n")).strip().upper()
+            woman += 1
+
+    # Let's see if the user wants to continue
+    choice = str(input("Continue? [Y / N] \n")).strip().upper()
     if choice == "N":
-        print(f"Pessoas com mais de 18: {add_age}.")
-        print(f"Homens cadastrados: {add_men}")
-        print(f"Mulheres com menos de 20 anos: {add_women}")
+        print(f'People over 18 years: {add_major}.')
+        print(f'Total men registered: {men}')
+        print(f'Number of women under 20: {woman}')
         break
