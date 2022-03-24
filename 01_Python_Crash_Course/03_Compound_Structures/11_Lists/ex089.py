9973e4b446d368151e432ae7726186d1ffba82ea
Marcos Henrique <marcoshsq@gmail.com>
2022-03-24 03:11:56 -0300

    Update ex089.py

---
+++ 01_Python_Crash_Course/03_Compound_Structures/11_Lists/ex089.py
@@ -0,0 +1,6 @@
+# Exercise 089 - Bulletin with Composite Lists
+
+'''Create a program that reads the name and two grades of multiple students 
+and stores them all in a composite list. At the end, show a report card 
+containing the average of each one and allow the user to show the grades 
+of each student individually.'''
