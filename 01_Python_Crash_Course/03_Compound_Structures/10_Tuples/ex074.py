8e119acf10edd8499849689b653c3a36494c45ad
Marcos Henrique <marcoshsq@gmail.com>
2022-03-24 00:35:04 -0300

    Exercise 074 - solved

--- 01_Python_Crash_Course/03_Compound_Structures/10_Tuples/ex074.py
+++ 01_Python_Crash_Course/03_Compound_Structures/10_Tuples/ex074.py
@@ -1,3 +1,5 @@
+from random import randint
+
 # Exercise 074 - Largest and Smallest Tuple Values
 
 '''Create a program that will generate five random numbers 
@@ -5,3 +7,11 @@ and put them in a tuple.After that, show the list of
 generated numbers and also indicate the smallest and 
 largest value that are in the tuple.
 '''
+
+
+numbers = (randint(1, 10), randint(1, 10), randint(1, 10), randint(1, 10), randint(1, 10))
+print(f'The numbers drawn were: ', end='')
+for i in numbers:
+    print(f'{i} ', end='')
+print(f'\nThe highest value was {max(numbers)}')
+print(f'The smallest value was {min(numbers)}')
