c997cba921aee2caf4b6616dd28a2732c7d1291f
Marcos Henrique <marcoshsq@gmail.com>
2022-03-24 03:46:36 -0300

    Update ex107.py

--- 01_Python_Crash_Course/03_Compound_Structures/14_Modularization in Python/ex107.py
+++ 01_Python_Crash_Course/03_Compound_Structures/14_Modularization in Python/ex107.py
@@ -1,1 +1,5 @@
-#
+# Exercise 107 - Exercising Python modules
+
+'''Create a module called coin.py that has built-in functions increase(), 
+decrease(), double() and half(). Also write a program that imports 
+this module and uses some of these functions.'''
