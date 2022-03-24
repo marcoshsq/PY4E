a5691e31e7f007bcea456fcf7d7af857dcbae403
Marcos Henrique <marcoshsq@gmail.com>
2022-03-24 03:03:19 -0300

    Update ex081.py

---
+++ 01_Python_Crash_Course/03_Compound_Structures/11_Lists/ex081.py
@@ -0,0 +1,7 @@
+# Exercise 081 - Extracting data from a List
+
+'''Create a program that will read several numbers and put them in a list. 
+After that, show:
+A) How many numbers were entered.
+B) The list of values, sorted in descending order.
+C) Whether the value 5 was entered and is or is not in the list.'''
