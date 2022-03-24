bdeaec5d845a67c33ce824d8769455bb92c924b9
Marcos Henrique <marcoshsq@gmail.com>
2022-03-24 03:19:03 -0300

    Update ex091.py

---
+++ 01_Python_Crash_Course/03_Compound_Structures/12_Dictionaries/ex091.py
@@ -0,0 +1,6 @@
+# Exercise 091 - Dice Game in Python
+
+'''Create a program where 4 players roll a dice and get random results. 
+Store these results in a Python dictionary. In the end, put that dictionary 
+in order, knowing that the winner rolled the highest number on the die.
+'''
