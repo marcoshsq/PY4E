38af0dbcd7f4957bc537b89b44449fc1cf1949de
Marcos Henrique <marcoshsq@gmail.com>
2022-03-24 03:02:15 -0300

    Update ex078.py

---
+++ 01_Python_Crash_Course/03_Compound_Structures/11_Lists/ex078.py
@@ -0,0 +1,6 @@
+# Exercise 078 - Largest and Smallest Values in the List
+
+'''Write a program that reads 5 numeric values and stores them in a list. 
+At the end, show which was the highest and 
+lowest value entered and their respective positions in the list.
+'''
