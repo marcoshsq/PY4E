38b62918bbe263fcb4476c630b1117800f799d38
Marcos Henrique <marcoshsq@gmail.com>
2022-03-24 04:58:25 -0300

    Update ex117.py

---
+++ 01_Python_Crash_Course/03_Compound_Structures/16_Classes/ex117.py
@@ -0,0 +1,13 @@
+# Exercise 117 - Rectangle Class
+
+'''Create a class that models a rectangle:
+
+Attributes: SideA, SideB (or Length and Width, or Base and Height, to choose)
+
+Methods: Change value of sides, Return value of sides, calculate Area and calculate Perimeter;
+
+Create a program that uses this class. It should ask the user to enter the 
+measurements of a location. Then, you must create an object with the 
+measurements and calculate the amount of floors and baseboards 
+needed for the place.
+'''
