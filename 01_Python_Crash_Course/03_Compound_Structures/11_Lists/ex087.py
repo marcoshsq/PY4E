539933dbbe7239f93b5312a0281473b9b21acf67
Marcos Henrique <marcoshsq@gmail.com>
2022-03-24 03:10:45 -0300

    Update ex087.py

---
+++ 01_Python_Crash_Course/03_Compound_Structures/11_Lists/ex087.py
@@ -0,0 +1,6 @@
+# Exercise 087 - More on Array in Python
+
+'''Enhance the previous challenge by showing at the end:
+A) The sum of all even values entered.
+B) The sum of the values in the third column.
+C) The largest value of the second row.'''
