87fdfb0f083fc80493a04112abcb54c1f70b7e7c
Marcos Henrique <marcoshsq@gmail.com>
2022-03-24 05:01:12 -0300

    Update ex119.py

---
+++ 01_Python_Crash_Course/03_Compound_Structures/16_Classes/ex119.py
@@ -0,0 +1,7 @@
+# Exercise 119 - Current Account Class
+
+'''Create a class to implement a current account. The class must have the 
+following attributes: account number, account holder name and balance. 
+The methods are as follows: changeName, deposit and withdrawal; In the constructor, 
+balance is optional, with a default value of zero and the other attributes are mandatory.
+'''
