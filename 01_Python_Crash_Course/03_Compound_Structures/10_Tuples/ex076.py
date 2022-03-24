115b5233d2787e4056e12f2478df3c3f765494a7
Marcos Henrique Silva Quirino <marcoshsq@gmail.com>
2022-03-24 17:48:03 -0300

    Exercise - 76 - solved

--- 01_Python_Crash_Course/03_Compound_Structures/10_Tuples/ex076.py
+++ 01_Python_Crash_Course/03_Compound_Structures/10_Tuples/ex076.py
@@ -4,3 +4,41 @@
 names and their respective prices, in sequence. At the end, 
 show a price listing, organizing the data in tabular form.
 """
+
+product_list = (
+    "Caderno",
+    7,
+    "Lápis",
+    3,
+    "Borracha",
+    0.75,
+    "Canetas",
+    1.50,
+    "Apontador",
+    2.50,
+    "Réguas",
+    3,
+    "Papel Sulfite",
+    7,
+    "Mochila",
+    55,
+    "Calendário",
+    15,
+    "Livros",
+    155,
+    "Notebook",
+    2500,
+    "Agenda",
+    25,
+)
+
+print("=" * 39)
+print(f"{'PRICE TABLE'}:^40")
+print("=" * 39)
+
+for i in range(0, len(product_list)):
+    if i % 2 == 0:
+        print(f"{product_list[i]:.<30}", end="")
+    else:
+        print(f"R$ {product_list[i]:>7.2f}")
+print("=" * 39)
