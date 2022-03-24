25e6041ca64df26a1a2a727a264866bfe868ecbd
Marcos Henrique <marcoshsq@gmail.com>
2022-03-24 03:02:59 -0300

    Update ex080.py

---
+++ 01_Python_Crash_Course/03_Compound_Structures/11_Lists/ex080.py
@@ -0,0 +1,5 @@
+# Exercise 080 - Ordered list without repetitions
+
+'''Create a program where the user can type five numeric values and register 
+them in a list, already in the correct insertion position 
+(without using sort()). At the end, show the sorted list on the screen.'''
