8e124f27569f440178f3e54750d684b1dae1f22a
Marcos Henrique <marcoshsq@gmail.com>
2022-03-24 03:46:51 -0300

    Update ex108.py

---
+++ 01_Python_Crash_Course/03_Compound_Structures/14_Modularization in Python/ex108.py
@@ -0,0 +1,4 @@
+# Exercise 108 - Formatting Coins in Python
+
+'''Adapt the code from challenge #107 by creating an additional function 
+called currency() that can display the numbers as a formatted currency value.'''
