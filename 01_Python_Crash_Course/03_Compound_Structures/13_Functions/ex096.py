a3e50f27a665b189c65ce34c23fab8706759bb58
Marcos Henrique <marcoshsq@gmail.com>
2022-03-24 03:32:11 -0300

    Update ex096.py

---
+++ 01_Python_Crash_Course/03_Compound_Structures/13_Functions/ex096.py
@@ -0,0 +1,4 @@
+# Exercise 096 - Function that calculates area
+
+'''Write a program that has a function called area(), which receives the 
+dimensions of a rectangular plot (width and length) and displays the area of the plot.'''
