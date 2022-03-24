ebd11322c062a692fc46bdc8f87b4ca7e41775e7
Marcos Henrique <marcoshsq@gmail.com>
2022-03-24 03:03:36 -0300

    Update ex082.py

---
+++ 01_Python_Crash_Course/03_Compound_Structures/11_Lists/ex082.py
@@ -0,0 +1,6 @@
+# Exercise 082 - Splitting values into multiple lists
+
+'''Create a program that will read several numbers and put them in a list. 
+After that, create two extra lists that will contain only the even values 
+and the odd values entered, respectively. At the end, show the content 
+of the three generated lists.'''
