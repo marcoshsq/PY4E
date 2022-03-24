70528a1c70c0c4f50eb777eee7c8232f0e8dbee6
Marcos Henrique <marcoshsq@gmail.com>
2022-03-24 00:49:18 -0300

    Exercise 075 - solved

--- 01_Python_Crash_Course/03_Compound_Structures/10_Tuples/ex075.py
+++ 01_Python_Crash_Course/03_Compound_Structures/10_Tuples/ex075.py
@@ -7,3 +7,24 @@ A) How many times did the value 9 appear.
 B) In which position was the first value entered 3.
 C) What were the even numbers.
 '''
+
+n = (int(input('Enter a number: ')), int(input('Enter another number: ')),
+      int(input('One more, almost there, I promise: ')), int(input('The last one: ')))
+
+print(f'You entered these numbers: {n}')
+
+# A)
+print(f'The number 9 appeared {n.count(9)} times')
+
+# B)
+if 3 in n:
+    print(f'The number 3 appeared at the {n.index(3) + 1}ª position')
+
+else:
+    print('You have not entered any number 3')
+
+# C)
+print('The even values were ', end='')
+for i in n:
+    if i % 2 == 0:
+        print(i, end='')
