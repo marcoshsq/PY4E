948e12aa8ea1c388d08063371a4ca577be4b8d8b
Marcos Henrique <marcoshsq@gmail.com>
2022-03-24 03:09:32 -0300

    Update ex084.py

---
+++ 01_Python_Crash_Course/03_Compound_Structures/11_Lists/ex084.py
@@ -0,0 +1,8 @@
+# Exercise 084 - Composite List and Data Analysis
+
+'''Make a program that reads the name and weight of several people, 
+keeping everything in a list. At the end, show:
+
+A) How many people were registered.
+B) A list with the heaviest people.
+C) A list with the lightest people.'''
