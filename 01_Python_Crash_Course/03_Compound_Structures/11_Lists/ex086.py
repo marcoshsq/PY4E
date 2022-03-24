ae49227ab2596bb0aaece2b4632e7e16f3c727a6
Marcos Henrique <marcoshsq@gmail.com>
2022-03-24 03:10:21 -0300

    Update ex086.py

---
+++ 01_Python_Crash_Course/03_Compound_Structures/11_Lists/ex086.py
@@ -0,0 +1,5 @@
+# Exercise 086 - Matrix in Python
+
+'''Create a program that declares a 3x3 dimensional matrix and fills it with 
+values read from the keyboard. At the end, show the matrix on the screen, 
+with the correct formatting.'''
