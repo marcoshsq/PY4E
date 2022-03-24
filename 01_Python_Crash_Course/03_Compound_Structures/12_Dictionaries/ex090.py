3cd3450fe0a95497ce188b89ed5c65c8796be974
Marcos Henrique <marcoshsq@gmail.com>
2022-03-24 03:18:47 -0300

    Update ex090.py

---
+++ 01_Python_Crash_Course/03_Compound_Structures/12_Dictionaries/ex090.py
@@ -0,0 +1,4 @@
+# Exercise 090 - Python Dictionary
+
+'''Make a program that reads a student's name and average, also storing the 
+situation in a dictionary. At the end, show the content of the structure on the screen.'''
