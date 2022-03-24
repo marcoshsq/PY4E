47a83c61b6f5e756fea26264624bab0af73115f9
Marcos Henrique <marcoshsq@gmail.com>
2022-03-24 03:03:54 -0300

    Update ex083.py

---
+++ 01_Python_Crash_Course/03_Compound_Structures/11_Lists/ex083.py
@@ -0,0 +1,5 @@
+# Exercise 083 - Validating Math Expressions
+
+'''Create a program where the user types any expression that uses parentheses. 
+Your application should analyze whether the passed expression has 
+the open and closed parentheses in the correct order.'''
