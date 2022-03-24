842a8338e033b19072406325021146c49f0cfbff
Marcos Henrique <marcoshsq@gmail.com>
2022-03-24 04:59:47 -0300

    Update ex118.py

---
+++ 01_Python_Crash_Course/03_Compound_Structures/16_Classes/ex118.py
@@ -0,0 +1,9 @@
+# Exercise 118 - Person Class
+
+'''Create a class that models a person:
+
+Attributes: name, age, weight and height
+
+Methods: Aging, getting fat, slimming, growing. Note: By default, for each year 
+that our person ages, if his age is less than 21 years old, he must grow 0.5 cm.
+'''
