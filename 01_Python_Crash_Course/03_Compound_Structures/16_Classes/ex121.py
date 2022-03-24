ac482b8f18feae5eec162cd12b7950657c0c83fa
Marcos Henrique <marcoshsq@gmail.com>
2022-03-24 05:03:46 -0300

    Update ex121.py

---
+++ 01_Python_Crash_Course/03_Compound_Structures/16_Classes/ex121.py
@@ -0,0 +1,11 @@
+# Exercise 121 - Virtual Pet Class
+
+'''Create a class that models a Tamagushi (Electronic Pet): 
+
+Attributes: Name, Hunger, Health and Age b. 
+Methods: Change Name, Hunger, Health and Age; Return Name, Hunger, Health and Age 
+Note: There is one more piece of information that we must take into account, 
+the Mood of our tamagushi, this mood is a combination of the Hunger and Health 
+attributes, that is, a calculated field, so we should not create a attribute 
+to store this information because it can be calculated at any time.
+'''
