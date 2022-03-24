69dd9ebc4eba3e6cfbcc7018d014645af780050c
Marcos Henrique <marcoshsq@gmail.com>
2022-03-24 03:11:39 -0300

    Update ex088.py

---
+++ 01_Python_Crash_Course/03_Compound_Structures/11_Lists/ex088.py
@@ -0,0 +1,6 @@
+# Exercise 088 - Guesses for the Mega Sena
+
+'''Write a program that helps a MEGA SENA player to create guesses. 
+The program will ask how many games will be generated and will 
+draw 6 numbers between 1 and 60 for each game, 
+registering everything in a composite list.'''
