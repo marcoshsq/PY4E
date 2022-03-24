8f794b80f1ddcfe7ed32444e909e667f795a42de
Marcos Henrique <marcoshsq@gmail.com>
2022-03-24 05:04:56 -0300

    Update ex122.py

---
+++ 01_Python_Crash_Course/03_Compound_Structures/16_Classes/ex122.py
@@ -0,0 +1,8 @@
+# Exercise 122 - Monkey Class
+
+'''Develop a Monkey class, which has the attributes name and stomach 
+(stomach) and at least the methods eat(), seeMouth() and digest(). 
+Make a program or test interactively, creating at least two monkeys, 
+feeding them at least 3 different foods and checking the stomach contents at each meal. 
+Try making one monkey eat the other. Is it possible to breed a cannibal monkey?
+'''
