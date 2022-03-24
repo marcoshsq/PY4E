8e1608c5f2748287098e53fca5e4cd6776c64d7f
Marcos Henrique <marcoshsq@gmail.com>
2022-03-24 03:33:13 -0300

    Update ex100.py

---
+++ 01_Python_Crash_Course/03_Compound_Structures/13_Functions/ex100.py
@@ -0,0 +1,6 @@
+# Exercise 100 - Functions to sort and add
+
+'''Write a program that has a list called numbers and two functions called 
+raffle() and sumPair(). The first function will select 5 numbers and place 
+them inside the list and the second function will show the sum of all even 
+values drawn by the previous function.'''
