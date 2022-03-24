873e15b9e13c836c7654794b1f2fce4f19c8b473
Marcos Henrique <marcoshsq@gmail.com>
2022-03-24 00:25:15 -0300

    Exercise 072 - solved

--- 01_Python_Crash_Course/03_Compound_Structures/10_Tuples/ex072.py
+++ 01_Python_Crash_Course/03_Compound_Structures/10_Tuples/ex072.py
@@ -12,12 +12,9 @@ number_extension = ('zaro', 'one', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven
                     'nineteen', 'Twenty')
 
 
-for i in number_extension:
-    count= int(input('Enter a number: '))
-    
-    for j in range((count + 1) - count):
-        print(f'You entered the number {count}, that written in full is {number_extension[count]}')
-    
-    control = str(input('Wish to continue: [Yes or No]')).strip()
-    if control in 'Nn':
+while True:
+    count = int(input('Enter a number between 0 and 20: '))
+    if 0<= count <= 20:
         break
+    print('Try again.', end='')
+print(f'You entered the number {number_extension[count]}')
