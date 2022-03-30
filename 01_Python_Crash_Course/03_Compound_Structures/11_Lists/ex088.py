9677a452722731a2471701777df1181496e422da
Marcos Henrique Silva Quirino <marcoshsq@gmail.com>
2022-03-30 16:58:07 -0300

    update

--- 01_Python_Crash_Course/03_Compound_Structures/11_Lists/ex088.py
+++ 01_Python_Crash_Course/03_Compound_Structures/11_Lists/ex088.py
@@ -4,3 +4,31 @@
 The program will ask how many games will be generated and will 
 draw 6 numbers between 1 and 60 for each game, 
 registering everything in a composite list.'''
+
+"""he Mega-Sena is the largest lottery in Brazil, 
+organised by the Caixa Econômica Federal bank since March 1996."""
+
+from random import randint
+from time import sleep
+list_a = list()
+games = list()
+quantity = int(input("How many games do you want me to draw? \n"))
+total = 1
+
+while total <= quantity:
+    counter = 0
+    while True:
+        number = randint(1, 60)
+        if number not in list_a:
+            list_a.append(number)
+            counter += 1
+        if counter >= 6:
+            break
+    list_a.sort()
+    games.append(list_a[:])
+    list_a.clear()
+    total += 1
+
+for i, l in enumerate(games):
+    print(f"Game {i + 1}: {l}")
+    sleep(1)
--- 01_Python_Crash_Course/03_Compound_Structures/11_Lists/ex089.py
+++ 01_Python_Crash_Course/03_Compound_Structures/11_Lists/ex089.py
@@ -1,6 +1,66 @@
 # Exercise 089 - Bulletin with Composite Lists
 
-'''Create a program that reads the name and two grades of multiple students 
+"""Create a program that reads the name and two grades of multiple students 
 and stores them all in a composite list. At the end, show a report card 
 containing the average of each one and allow the user to show the grades 
-of each student individually.'''
+of each student individually."""
+
+from statistics import mean
+
+infos_students = list()
+
+
+def get_name():
+    return input("Student name: \n")
+
+
+def get_grades(student_name):
+    grades = list()
+    counter = 1
+    grades.append(student_name)
+    while counter < 3:
+        grade = int(input(f"{counter}ª grade: \n"))
+        grades.append(grade)
+        counter += 1
+    infos_students.append(grades)
+    return infos_students
+
+
+def display_infos():
+    return infos_students
+
+
+while True:
+    get_grades(get_name())
+    question = input("Want to add another student? \n").lower()
+    if question[0] == "n":
+        # print(display_infos())
+        break
+
+"""
+Shows a bulletin containing the average of each
+and allow the user to show each student's grades individually
+"""
+
+
+def calculate_mean(students_infos):
+    students_means = list()
+    new_infos = list()  # bad naming
+
+    for item in students_infos:
+        students_means.append(item[0])
+        students_means.append(mean(item[1:]))
+        new_infos.append(students_means)
+        del students_means
+        students_means = list()
+
+    return new_infos
+
+
+def display_school_report():
+    pass
+
+
+# display_school_report(display_infos())
+
+print(calculate_mean(display_infos()))
