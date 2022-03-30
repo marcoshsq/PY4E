50e5da5f54e28dd75d875273e59e3e1226870724
Marcos Henrique Silva Quirino <marcoshsq@gmail.com>
2022-03-30 01:28:42 -0300

    update

--- 01_Python_Crash_Course/03_Compound_Structures/11_Lists/ex085.py
+++ 01_Python_Crash_Course/03_Compound_Structures/11_Lists/ex085.py
@@ -1,6 +1,5 @@
 # Exercise 085 - Odd and Even Lists
 
-'''Create a program where the user can enter seven numeric values and register 
+"""Create a program where the user can enter seven numeric values and register 
 them in a single list that keeps even and odd values separate. At the end, show 
-the even and odd values in ascending order.'''
-
+the even and odd values in ascending order."""
