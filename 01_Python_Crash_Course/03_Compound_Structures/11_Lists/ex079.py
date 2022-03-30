eb92a6258a4d4c56fb60a6047b0ac0fb338e0c5a
Marcos Henrique Silva Quirino <marcoshsq@gmail.com>
2022-03-29 23:04:59 -0300

    update

--- 01_Python_Crash_Course/03_Compound_Structures/11_Lists/ex079.py
+++ 01_Python_Crash_Course/03_Compound_Structures/11_Lists/ex079.py
@@ -1,5 +1,27 @@
 # Exercise 079 - Single Values in a List
 
 """Create a program where the user can enter several numeric values and register them in a list. 
-If the number already exists in there, it will not be added. At the end, 
-all unique values entered will be displayed, in ascending order."""
+If the number already exists in there, it will not be added. 
+At the end, all unique values entered will be displayed, in ascending order."""
+
+list_numbers = []
+
+while True:
+    number = int(input("Enter a number: \n"))
+    if number in list_numbers:
+        print("The number you enter is already on the list!")
+    else:
+        list_numbers.append(number)
+        print("The number you entered was added")
+
+    question = str(input("Want to continue? [Yes] or [No]\n")).strip().upper()
+
+    if question[0] == "N":
+        break
+    elif question == "Y":
+        print("Continuing on!...")
+    else:
+        print("Please, enter Yes or No!")
+
+list_numbers.sort()
+print(f"You entered the values: {list_numbers}!")
--- 01_Python_Crash_Course/03_Compound_Structures/11_Lists/ex083.py
+++ 01_Python_Crash_Course/03_Compound_Structures/11_Lists/ex083.py
@@ -3,3 +3,4 @@
 '''Create a program where the user types any expression that uses parentheses. 
 Your application should analyze whether the passed expression has 
 the open and closed parentheses in the correct order.'''
+
