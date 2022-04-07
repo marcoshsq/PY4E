984197772ed295d0380dacebcaf13a1289f4e477
Marcos Henrique Silva Quirino <marcoshsq@gmail.com>
2022-04-07 17:01:28 -0300

    update

--- 01_Python_Crash_Course/03_Compound_Structures/13_Functions/ex100.py
+++ 01_Python_Crash_Course/03_Compound_Structures/13_Functions/ex100.py
@@ -1,6 +1,8 @@
 # Exercise 100 - Functions to sort and add
 
-'''Write a program that has a list called numbers and two functions called 
+"""Write a program that has a list called numbers and two functions called 
 raffle() and sumPair(). The first function will select 5 numbers and place 
 them inside the list and the second function will show the sum of all even 
-values drawn by the previous function.'''
+values drawn by the previous function."""
+
+
