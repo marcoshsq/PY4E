b99f55b49197cf6cc42d5d4559aa9aa70a49659b
Marcos Henrique Silva Quirino <marcoshsq@gmail.com>
2022-05-16 23:03:55 -0300

    Course 04 - SQL

--- Course 04 - Using Databases with Python/Week 02/03. Counting Email in a Database.py
+++ Course 04 - Using Databases with Python/Week 02/03. Counting Email in a Database.py
@@ -26,3 +26,52 @@ The program can be speeded up greatly by moving the commit operation outside of 
 In any database program, there is a balance between the number of operations you execute 
 between commits and the importance of not losing the results of operations that have not 
 yet been committed."""
+
+
+# 1. Import sqlite3
+# 2. Call .connect() method to create connection object
+# 3. Create cursor object
+# 4. Delete table if it exists
+# 5. Create table with domains and counts as attributes
+# 6. Request file name
+# 7. Create file handle
+# 8. Loop through file to retrieve domain of emails
+# 9a. Retrieve data
+# 9b. Call fetchone() method to query db
+# 10. if/else statement similar to get()
+# 11. Commit changes with commit()
+# 12. Print counts
+
+import sqlite3
+
+conn = sqlite3.connect('emaildb.sqlite')
+cur = conn.cursor()
+
+cur.execute('DROP TABLE IF EXISTS Counts')
+
+cur.execute('CREATE TABLE Counts(org TEXT, count INTEGER)')
+
+fname = 'mbox.txt'
+fh = open(fname)
+for line in fh:
+    if not line.startswith('From: '):
+        continue
+    pieces = line.split()
+    email = pieces[1]
+    org = email.split('@')[1]
+    cur.execute('SELECT count FROM Counts WHERE org = ? ', (org,))
+    row = cur.fetchone()
+    if row is None:
+        cur.execute('''INSERT INTO Counts (org, count)
+                    VALUES (?, 1)''', (org,))
+    else:
+        cur.execute('UPDATE Counts SET count = count + 1 WHERE org = ?',
+                    (org,))
+conn.commit()
+
+sqlstr = 'SELECT org, count FROM Counts ORDER BY count DESC LIMIT 10'
+
+for row in cur.execute(sqlstr):
+    print(str(row[0]), row[1])
+
+cur.close()
